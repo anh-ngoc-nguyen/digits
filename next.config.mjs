c90d89a81f4aba62ed67966bcca21ed3078507dd
ayenaych <anhimu67@gmail.com>
2025-04-03 20:41:21 -1000

    contact forms finished in 33 minutes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 export default {
-    images: {
-      domains: ['github.com', 'avatars0.githubusercontent.com', 'www.ics.hawaii.edu'],
-    },
-  };
-  
+  images: {
+    domains: [
+      'github.com',
+      'avatars0.githubusercontent.com',
+      'www.ics.hawaii.edu',
+      'www2.hawaii.edu',
+    ],
+  },
+};
